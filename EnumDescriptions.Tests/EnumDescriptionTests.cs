029a1819570a56713e1924da2de667c2755505bc
henry <henry.eng@alaskaair.com>
2017-09-11 12:43:27 -0700

    Initial commit

---
+++ EnumDescriptions.Tests/EnumDescriptionTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace EnumDescriptions.Tests
+{
+    public class EnumDescriptionTests
+    {
+        [Test]
+        public void A319Description_Returns319()
+        {
+            AirbusEquipmentTypes a319 = AirbusEquipmentTypes.A319;
+            Assert.AreEqual("319", a319.GetDescription());
+        }
+
+        [Test]
+        public void A321String_IsFound()
+        {
+            AirbusEquipmentTypes a321 = AirbusEquipmentTypes.A321;
+
+            Assert.IsTrue(FindMatch("321"));
+        }
+
+        [Test]
+        public void A321xyzString_IsNotFound()
+        {
+            AirbusEquipmentTypes a321 = AirbusEquipmentTypes.A321;
+
+            Assert.IsFalse(FindMatch("321xyz"));
+        }
+
+        private bool FindMatch(string equipmentType)
+        {
+            return Enum.GetValues(typeof(AirbusEquipmentTypes)).OfType<AirbusEquipmentTypes>().Any(f => f.GetDescription().Equals(equipmentType));
+        }
+    }
+
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum source)
+        {
+            FieldInfo fi = source.GetType().GetField(source.ToString());
+
+            System.ComponentModel.DescriptionAttribute[] attributes = (System.ComponentModel.DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(System.ComponentModel.DescriptionAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Description : source.ToString();
+        }
+    }
+}
---
+++ EnumDescriptions/AirbusEquipmentTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace EnumDescriptions
+{
+    public enum AirbusEquipmentTypes
+    {
+        [System.ComponentModel.Description("319")]
+        A319,
+        [System.ComponentModel.Description("320")]
+        A320,
+        [System.ComponentModel.Description("321")]
+        A321,
+        [System.ComponentModel.Description("32B")]
+        A32B,
+        [System.ComponentModel.Description("32S")]
+        A32S,
+        [System.ComponentModel.Description("32A")]
+        A32A
+    }
+}
